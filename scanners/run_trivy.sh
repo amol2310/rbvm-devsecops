cceee96e64a3532d4b54dc5f97d7e8b173e2e956
Amol Marathe <maratheamol2310@gmail.com>
2025-04-20 16:32:17 +0530

    updated run_trivy.sh

--- scanners/run_trivy.sh
+++ scanners/run_trivy.sh
@@ -1,4 +1,6 @@
-TARGET_IMAGE=$1
+#!/bin/bash
+
+TARGET_IMAGE="$1"
 echo "Running trivy scan on vulnerable application"
 mkdir -p scanners/sample_output
-trivy image -f json -o scanners/sample_output/trivy_output.json $TARGET_IMAGE
+trivy image -f json -o scanners/sample_output/trivy_output.json "$TARGET_IMAGE"
